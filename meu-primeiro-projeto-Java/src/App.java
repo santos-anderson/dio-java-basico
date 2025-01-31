64b46f469f93431ef877e1cd00bcaf6114b85c90
Anderson Alves Santos <santos.anders@gmail.com>
2025-01-31 19:35:29 +0000

    mantendo

--- meu-primeiro-projeto-Java/src/App.java
+++ meu-primeiro-projeto-Java/src/App.java
@@ -1,5 +1,5 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("boas-vindas turma dio");
+    public static void main(String[] args) {
+        System.out.println ("ola turma");
     }
 }
